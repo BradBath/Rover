915fdae63dc3b47627448b8e0dc96f3cfede0b3c
Bradley Bath <badfitz66@gmail.com>
2020-03-25 19:38:05 +0000

    Rearranged file structure and started work on gameplay
    Rigged up the rover with joints so it moves via rigidbody physics and added a basic 'Rover' script that will serve as the controller for the Rover

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,55 @@
+﻿
+using System;
+using System.Linq;
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Controller class that deals with most of the game logic. It does, but is not limited to:
+    /*
+     * Sending input to arduino
+     * Recieving input from arduino
+     * Parsing commands
+     * Running commands
+     * 
+     */
+/// </summary>
+
+public class Controller : MonoBehaviour
+{
+    public SerialController serialController;
+    bool canSendInput = true;
+    // Initialization
+    void Start()
+    {
+        serialController = GameObject.Find("SerialController").GetComponent<SerialController>();
+
+    }
+
+    IEnumerator delay()
+    {
+        canSendInput = false;
+        yield return new WaitForSeconds(1);
+        canSendInput = true;
+    }
+    void OnMessageArrived(string msg)
+    {
+        print("Messaged recieved from Arduino " + msg);
+    }
+
+
+    void OnConnectionEvent(bool success)
+    {
+        print("Connected to Arduino? " + success);
+    }
+
+    // Executed each frame
+    void Update()
+    {
+        if (Input.inputString != "")
+        {
+            int asciiCode = System.Convert.ToInt32(Input.inputString[0]);
+            serialController.SendSerialMessage(Input.inputString.Substring(0, 1));
+        }
+    }
+}
---
+++ Assets/Scripts/Rover.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rover : MonoBehaviour
+{
+
+    [SerializeField] HingeJoint frontLeftWheel;
+    [SerializeField] HingeJoint frontRightWheel;
+    [SerializeField] Transform frontLeftWheelBracket;
+    [SerializeField] Transform frontRightWheelBracket;
+
+
+    public void Turn(float deg)
+    {
+        print("Turning " + deg.ToString() + " degrees");
+        frontLeftWheelBracket.rotation = Quaternion.Euler(0, deg, 0);
+        frontRightWheelBracket.rotation = Quaternion.Euler(0, deg, 0);
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            Turn(75);
+        }
+        else if (Input.GetAxis("Horizontal") < 0)
+        {
+            Turn(-75);
+        }
+        else if (Input.GetAxis("Horizontal") == 0)
+        {
+            Turn(0);
+        }
+    }
+}
